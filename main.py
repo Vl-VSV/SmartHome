9b028a6454506315807186a9c34c99b015fe7a62
Vl-VSV <vvs_2015@icloud.com>
2023-04-19 17:39:08 +0300

    new commands

--- main.py
+++ main.py
@@ -9,66 +9,14 @@ app = FastAPI(
     title="Trading app"
 )
 
-fake_users = [
-    {'id': 1, "role": "admin", "name": "Bob"},
-    {'id': 2, "role": "investor", "name": "John"},
-    {'id': 3, "role": "trader", "name": "Matt"},
-    {"id": 4, "role": "investor", "name": "Homer", "degree": [
-        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
-    ]}
-]
 
-fake_trades = [
-    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
-    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 125, "amount": 2.12},
-]
+ledState = 1
 
+@app.get('/getLedState', response_model=int)
+def get_led_state():
+    return ledState
 
-class DegreeType(Enum):
-    newbie = 'newbie'
-    expert = 'expert'
-
-
-class Degree(BaseModel):
-    id: int
-    created_at: datetime
-    type_degree: DegreeType
-
-
-class User(BaseModel):
-    id: int
-    role: str
-    name: str
-    degree: Optional[List[Degree]] = []
-
-
-@app.get('/users/{user_id}', response_model=List[User])
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get('id') == user_id]
-
-
-@app.get('/trades')
-def get_trades(limit: int = 1, offset: int = 0):
-    return fake_trades[offset:][:limit]
-
-
-@app.post('/users/{user_id}')
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users))[0]
-    current_user['name'] = new_name
-    return {'status': 200, 'data': current_user}
-
-
-class Trade(BaseModel):
-    id: int
-    user_id: int
-    currency: str = Field(max_length=5)
-    side: str
-    price: int = Field(ge=0)
-    amount: float
-
-
-@app.post("/trades")
-def add_trades(trades: List[Trade]):
-    fake_trades.extend(trades)
-    return {'status': 200, 'data': fake_trades}
+@app.post('/setLedState/{state}')
+def set_led_state(state: int):
+    ledState = state
+    return {'status': 200, 'newLedState' : ledState}
